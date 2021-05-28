4485b84fa20ac74c0240c0b8bcbaf4dfe0e702dd
Martin Spassov <mspassov@uwo.ca>
2021-05-28 11:19:05 -0400

    html and JS added

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container');
+const text = document.querySelector('.text');
+
+const totalTime = 8000;
+const breatheTime = (totalTime / 5) * 2;
+const holdTime = (totalTime / 5);
+
+animationFunc();
+setInterval(animationFunc, totalTime);
+
+function animationFunc(){
+    text.innerHTML = 'Breathe In';
+    container.className = 'container grow';
+
+    setTimeout(() =>{
+        text.innerHTML = 'Hold';
+        setTimeout(() =>{
+            text.innerHTML = 'Breathe Out'
+            container.className = 'container shrink';
+        }, holdTime)
+
+    }, breatheTime)
+}
+
